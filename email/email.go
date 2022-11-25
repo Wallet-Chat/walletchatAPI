5d8ce78a109be59605e592434669b3de31140876
Cyber Kevin <emailkevinlarson@gmail.com>
2022-11-24 22:26:32 -0700

    using email module now

--- email/email.go
+++ email/email.go
@@ -1,6 +1,6 @@
 package email
 
-func notificationEmail() string {
+func NotificationEmail(DMs string, NFTs string, Community string) string {
 	return `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
