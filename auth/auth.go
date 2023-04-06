703ad968d809d5d03bb1a758483a97a3cb101406
Cyber Kevin <emailkevinlarson@gmail.com>
2023-04-05 22:41:29 -0700

    revert

--- auth/auth.go
+++ auth/auth.go
@@ -205,12 +205,11 @@ func UserNonceHandler() http.HandlerFunc {
 		}
 
 		//combining /register and /users (no need to call both and check each time)
-		// nonce, err := GetNonce()
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	return
-		// }
-		nonce := "staging_test"
+		nonce, err := GetNonce()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		user := Authuser{
 			Address: address, // let's only store lower case
 			Nonce:   nonce,
@@ -575,11 +574,10 @@ func Authenticate(walletName string, address string, nonce string, message strin
 	}
 
 	// update the nonce here so that the signature cannot be resused
-	// nonce, err = GetNonce()
-	// if err != nil {
-	// 	return Authuser, err
-	// }
-	nonce = "staging_test"
+	nonce, err = GetNonce()
+	if err != nil {
+		return Authuser, err
+	}
 	Authuser.Nonce = nonce
 	Update(Authuser)
 
