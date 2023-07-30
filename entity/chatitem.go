c2c877eaf12b5655053efdbb2e7da91d0136e74d
Cyber Kevin <emailkevinlarson@gmail.com>
2023-07-30 19:13:25 +0300

    updates preparing for new community updates

--- entity/chatitem.go
+++ entity/chatitem.go
@@ -136,9 +136,26 @@ type CommunitySocialStruct struct {
 	Name string `json:"name"`
 }
 
+//cant use camel notation because gorm adds "_" for any subsequent capital letter
+type Communityaccesscondition struct {
+	Id      int    `gorm:"primaryKey;autoIncrement"`
+	Slug    string `json:"slug"` //change community to slug in DB table too
+	Nftaddr string `json:"address"`
+	Count   string `json:"count"`
+}
 type Createcommunityitem struct {
-	Community string                  `json:"community"`
-	Social    []CommunitySocialStruct `json:"social"`
+	Id     int                     `gorm:"primaryKey;autoIncrement"`
+	Name   string                  `json:"name"`
+	Slug   string                  `json:"slug"`
+	Image  string                  `json:"image"` //base64
+	Social []CommunitySocialStruct `json:"social"`
+}
+
+type Communityadmin struct {
+	Id          int    `gorm:"primaryKey;autoIncrement"`
+	Slug        string `json:"slug"`
+	Adminaddr   string `json:"adminaddr"`   //wallet addr
+	Accesslevel string `json:"accesslevel"` //admin, moderator, ?
 }
 
 type BookmarkReturnItem struct {
