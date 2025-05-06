e7cc1faa497aa995d4c62c745a2011a4ec823d18
Kevin Larson <emailkevinlarson@gmail.com>
2025-05-05 22:17:55 -0700

    remove print

--- auth/auth.go
+++ auth/auth.go
@@ -612,7 +612,7 @@ func AuthMiddleware(jwtProvider *JwtHmacProvider) func(next http.Handler) http.H
 			}
 			claims, err := jwtProvider.Verify(tokenString)
 			if err != nil {
-				fmt.Println("kevin test here error:  ", err)
+				//fmt.Println("kevin test here error:  ", err)
 				w.WriteHeader(http.StatusUnauthorized)
 				return
 			}
